2e871d273689eed948ebb0f572bddbe6574508ad
Bluelich <bluelich@qq.com>
2018-01-12 12:27:25 +0800

    update

--- centos.sh
+++ centos.sh
@@ -11,10 +11,9 @@ rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm;
 curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo;
 yum -y install yum-utils;
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo;
-yum install -y epel-release ruby git curl file python-setuptools bash-completion bash-completion-extras zsh vsftpd perl-ExtUtils-Embed perl-Digest-MD5 iptables-services httpd php php-mysql php-gd libjpeg php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-bcmath php-mhash go node rust mysql-community-server ctags gcc rpm rpm-devel rpm-build-libs rpmdevtools zlib zlib-devel openresty luajit  luajit-devel gperftools gd-devel libxslt-devel openssl openssl-devel pcre pcre-devel clang clang-devel cmake python;
+yum install -y epel-release ruby git curl file python-setuptools bash-completion bash-completion-extras vsftpd perl-ExtUtils-Embed perl-Digest-MD5 iptables-services httpd php php-mysql php-gd libjpeg php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-bcmath php-mhash go rust mysql-community-server ctags gcc rpm rpm-devel rpm-build-libs rpmdevtools zlib zlib-devel openresty luajit  luajit-devel gperftools gd-devel libxslt-devel openssl openssl-devel pcre pcre-devel clang clang-devel cmake python;
 yum provides -y geoip-devel;
 yum install -y geoip-devel;
-echo "Development Tools******************************************";
 yum groupinstall -y 'Development Tools';
 yum remove -y vim-minimal;
 yum update -y vim;
@@ -25,4 +24,5 @@ wget -P ~ https://jaist.dl.sourceforge.net/project/vimcdoc/vimcdoc/vimcdoc-2.1.0
 rm -rf ~/.vim/bundle;
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 # cp $(path)/motd /etc/motd;
+yum -y install zsh;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
