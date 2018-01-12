116444ed7c26b4ff5804a3baf86d39fe07c71e5e
Bluelich <bluelich@qq.com>
2018-01-12 12:25:13 +0800

    debug

--- centos.sh
+++ centos.sh
@@ -14,6 +14,7 @@ yum-config-manager --add-repo https://openresty.org/package/centos/openresty.rep
 yum install -y epel-release ruby git curl file python-setuptools bash-completion bash-completion-extras zsh vsftpd perl-ExtUtils-Embed perl-Digest-MD5 iptables-services httpd php php-mysql php-gd libjpeg php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-bcmath php-mhash go node rust mysql-community-server ctags gcc rpm rpm-devel rpm-build-libs rpmdevtools zlib zlib-devel openresty luajit  luajit-devel gperftools gd-devel libxslt-devel openssl openssl-devel pcre pcre-devel clang clang-devel cmake python;
 yum provides -y geoip-devel;
 yum install -y geoip-devel;
+echo "Development Tools******************************************";
 yum groupinstall -y 'Development Tools';
 yum remove -y vim-minimal;
 yum update -y vim;
